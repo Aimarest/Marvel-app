73d151b0e2b9b8d3e68944c29f5bcd0bee2f085e
Ana <amarcosestevez@gmail.com>
2022-07-28 20:30:13 +0200

    add margin at  moon icon

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -6,15 +6,15 @@ const Switch = (props) => {
   }
   return (
     <div className="switchComponent">
-  <i class="fa-solid fa-sun"></i>
-      
+ 
+      <i class="fa-solid fa-sun"></i>
       <input
         className="react-switch-checkbox"
         id={`react-switch-new`}
         type="checkbox"
 
       />
-   
+    
       <label
         className="react-switch-label"
         htmlFor={`react-switch-new`}
